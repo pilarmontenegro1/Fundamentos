21a8f0e8a69d62ce82785d297da94c741490191c
pilarmontenegro1 <69353164+pilarmontenegro1@users.noreply.github.com>
2020-08-14 09:39:35 -0400

    Add files via upload

---
+++ Sumadosvariables.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+#include "conio.h"
+
+int main()
+{
+    int a,b,c;
+    cout<<"Ingresar el primer operando: ";
+    cin>>a;
+    cout<<"Ingresar el segundo operando: ";
+    cin>>b;
+    c=a+b;
+    cout<<"La sumatoria es: "<<c;
+    getch();
+  }
+
+
+
---
+++ Valorabsoluto.psc
@@ -0,0 +1,11 @@
+Algoritmo  ValorAbsoluto
+	Definir  x,z Como Entero
+	Escribir "Ingresar x: ";
+	Leer x; // x es el numero a calcular su valor absoluto
+	Si (x>=0) Entonces
+		z<-x;
+	SiNo
+		z<--x;
+	FinSi
+	Escribir "El valor absoluto es: ", z;
+FinAlgoritmo
